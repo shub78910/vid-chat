248c35d854b2611c25900a52911248b475555297
shubham-aza <shubham@azafashions.com>
2025-07-06 02:11:31 +0530

    remove turn keys

--- client/src/components/VideoRoom.tsx
+++ client/src/components/VideoRoom.tsx
@@ -39,9 +39,6 @@ const VideoRoom = () => {
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
         { urls: 'stun:stun1.l.google.com:19302' },
-        { urls: 'turn:openrelay.metered.ca:80' },
-        { urls: 'turn:openrelay.metered.ca:443' },
-        { urls: 'turn:openrelay.metered.ca:443?transport=tcp' }
       ]
     });
 
